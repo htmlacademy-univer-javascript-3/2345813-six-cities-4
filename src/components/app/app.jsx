fb16da4260c42d0635fb0c82d9865b7bed15a5d3
Flimill <uhovad03@gmail.com>
2024-03-05 16:50:13 +0300

    module2-task1

---
+++ src/components/app/app.jsx
@@ -0,0 +1,22 @@
+import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
+import React from "react";
+
+
+const App = (props) => {
+  const {offersCount, offersNames} = props;
+
+  return (
+    <Main
+      offersCount={offersCount}
+      offersNames={offersNames}
+    />
+  );
+};
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+  offersNames: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired
+};
+
+export default App;
